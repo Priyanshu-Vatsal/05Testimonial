97ed46701f19d898071aa057d96d73effb4386ef
Priyanshu Vatsal <8priyanshuvatsal@gmail.com>
2024-05-23 12:19:33 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Testimonial from './component/Testimonial'
+import reviews from './ReviewData'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      
+    <div className='flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200'>
+      <div className='text-center'>
+        
+        <h1 className='text-4xl font-bold'>Our Testimonial</h1>
+        
+        <div className='bg-violet-400 h-[4px] w-1/5 mt-3 mx-auto'></div>
+
+        <Testimonial reviews={reviews}></Testimonial>
+      </div>
     </div>
   )
 }
